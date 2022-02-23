a5cb2559e20becf4fbbb922ab16876d766574e5c
Zach Nash <afreakenturtle@gmail.com>
2022-02-23 00:12:28 -0500

    Got Adoptable Pet API Working

--- index.js
+++ index.js
@@ -1,24 +1,47 @@
 //Called function for generating the card for adoptable dogs
+console.log('im here');
 
 function renderDog(animalsArray) {
-    let dogHTML = animalsArray.map(currentDog => {
-        return `<div class="card text-white bg-dark" style="width: 18rem;">
-                        <img src="${currentDog.photo}" class="card-img-top" alt="...">
-                        <div class="card-body">
-                            <h5 class="card-title">${currentDog.breeds.primary}</h5>
-                            <p class="card-text">${currentDog.age}</p>
-                            <a href="#" class="btn btn-primary">Learn More About</a>
-                        </div>
-                </div>`
-    })
-    return dogHTML.join('');
+  console.log('data fetch', animalsArray)
+  const dConInner = document.createElement("div")   
+  let dogHTML = animalsArray.map(currentDog => {
+
+    if (currentDog.primary_photo_cropped === null || currentDog.primary_photo_cropped.small === '') {
+      let dogCard =  `<div class="card text-white bg-dark" style="width: 18rem;">
+                          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQj_vJblJLSn59TSwKbG3-CawQ39uUHDX3vf0nX8q48Mr8anv5aBai_OGk2HrtqlVh1sy0&usqp=CAU" class="card-img-top" alt="...">
+                          <div class="card-body">
+                              <h5 class="card-title">${currentDog.breeds.primary}</h5>
+                              <p class="card-text">${currentDog.age}</p>
+                              <a href="#" class="btn btn-primary">Adopt Me</a>
+                          </div>
+                        </div>`
+        dogsCon.innerHTML += dogCard 
+    } else {
+      let dogCard =  `<div class="card text-white bg-dark" style="width: 18rem;">
+                          <img src="${currentDog.primary_photo_cropped.small}" class="card-img-top" alt="...">
+                          <div class="card-body">
+                              <h5 class="card-title">${currentDog.breeds.primary}</h5>
+                              <p class="card-text">${currentDog.age}</p>
+                              <a href="#" class="btn btn-primary">Adopt Me</a>
+                          </div>
+                        </div>`
+        dogsCon.innerHTML += dogCard     
+    }
+
+    }).join('');
+
+    // dogsCon.appendChild(dogHTML);
+    // return dogHTML.join('');
 }
 
 //THis is the API for the Adoptable pets on the page
-let dcard = document.getElementById('dcard');
+let dogsCon = document.getElementById('Dogs-Container');
+let search = document.getElementById('search-form');
+let search_bar = document.getElementById('search-text');
+let searchBtn = document.getElementById('search_button');
 
 var myHeaders = new Headers();
-myHeaders.append("Authorization", "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiJ2MGk4djVyZzFVZGZITnZwQ3FzVlhCUWRjUFlOOXdtZkk5ZVpPaURVejNOT1VaQkhGWSIsImp0aSI6ImFkYjc4ODA3OWE1M2UyZjQyNDkxODUxMTA5YWQ5MTJmOWNiODA5ZDM2YmNiNzFmYzA1YzMzMjI2Njk4YjVhZTUyMzc5ZmY3ZTNhMzkxN2ExIiwiaWF0IjoxNjQ1NDg0Nzk4LCJuYmYiOjE2NDU0ODQ3OTgsImV4cCI6MTY0NTQ4ODM5OCwic3ViIjoiIiwic2NvcGVzIjpbXX0.Ke4pT0r_pus_WPpJQLvewteSa_JPokojrHaiMWhRYEzjv1XgON84bIVm5jFKVdJtcTb4HkfoaLEpqCcP4QK-BA2JCrJZM8-py9Wad2mx4RClP5OA_jqzxEStx4XRT2P6_jNbBGQ2ISSeFdymfz1pYQWu0tELFKox5Lp_aza6aY66Z7Bk669oWnblXh2-mNSRVUxcQT8IkJ0ulCNkfiZPfTyyESOGKscREt6744IIVfx52pG7OjAyVN9VAKHyrGvHkQ8ikcUA8r4-bdbu6J7JKQS-23IC0Y2N-0tjNw0YrinvmrJ3tWCbMTV-gWuOXgW9x0uT0yFxtrQ_m7HuZ9jhrQ");
+myHeaders.append("Authorization", "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiJ2MGk4djVyZzFVZGZITnZwQ3FzVlhCUWRjUFlOOXdtZkk5ZVpPaURVejNOT1VaQkhGWSIsImp0aSI6ImM1Y2Q4ODM4YjI0M2VlMTc2YjA4ZjI4Y2Q3ZjNiOGMzYTA2ODgzZTZkZTBhYWY3Yzg1ZGRmMDExMzk4MmVkZDZiYmM0NmJhNTliYjFlMTUwIiwiaWF0IjoxNjQ1NTkwNzQ1LCJuYmYiOjE2NDU1OTA3NDUsImV4cCI6MTY0NTU5NDM0NSwic3ViIjoiIiwic2NvcGVzIjpbXX0.rFhc5fjqtTVx-NtEn6ip22GzAOCxyBmfm1kb-L3UbWPUU-GMtNrmuTVh-kUSJd10FEm_MyuiDzLTx0n4_lnguRwO3tflYIUr4PzRncddKqeAIOQbDxrKbZL5LYZYmjszZuO6Ydri7WlDXD6EStEFUdkSg3bnw_lLEterwbB4eSANIUTb6dB77hMVAR9HUQLVvfnHXxdjlj4tTFmULF5-R8yHP6soiY64SSGiqwd2qk4bZ0efJ4twp8UnBCMS5TdtD2-BIzFvBEDwzKZHU1KhPctkUBXqBWrIeaBSoqcj2ZRJ301yXKh8ilK0gUylLXmdOhYTRIVvzOzuPdcHPCPnmA");
 
 var requestOptions = {
   method: 'GET',
@@ -26,28 +49,31 @@ var requestOptions = {
   redirect: 'follow'
 };
 
-fetch("https://api.petfinder.com/v2/animals?type=dog&page=2", requestOptions)
-  .then(response => response.text())
-  .then(result => console.log(result))
-  .catch(error => console.log('error', error));
-
-
-  //This is the API for the random dog facts that will be appearing towards the top of the page.
-let btn = document.getElementById('btn');
-let ouput = document.getElementById('output');
-let facts = requestOptions;
+searchBtn.addEventListener('click', (event) => {
+  event.preventDefault();
+  console.log('search btn click');
+  let search_string = search_bar.value;
+  console.log('after search', search_string)
   
-  var requestOptions = {
-    method: 'GET',
-    redirect: 'follow'
-  };
+    // fetch(`https://api.petfinder.com/v2/animals?type=dog&location=${search_string}`, requestOptions)
+      fetch(`https://cors-anywhere.herokuapp.com/https://api.petfinder.com/v2/animals?type=dog&location=${search_string}`, requestOptions)
+      .then(response => response.json())
+      .then(function (data) {
+        console.log('data', data)
+      renderDog(data.animals)
+      });
+});
+
+  // //This is the API for the random dog facts that will be appearing towards the top of the page.
+  // var requestOptions = {
+  //   method: 'GET',
+  //   redirect: 'follow'
+  // };
   
-  fetch("https://dog-facts-api.herokuapp.com/api/v1/resources/dogs?number=1", requestOptions)
-    .then(response => response.text())
-    .then(result => console.log(result))
-    .catch(error => console.log('error', error));
+  // fetch("https://dog-facts-api.herokuapp.com/api/v1/resources/dogs?number=1", requestOptions)
+  //   .then(response => response.text())
+  //   .then(result => console.log(result))
+  //   .catch(error => console.log('error', error));
 
-    //When the button is clicked it call the dog facts api to generate a random dog fact
-  btn.addEventListener('click', function() {
-
-  });
+ 
+ 
